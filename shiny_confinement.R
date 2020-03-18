b890ed1a0b09ba57bbaa9f0840739d74f05fb5f6
Lafont <francois.lafont@ccomptes.fr>
2020-03-18 19:33:16 +0100

    update du 18/03/2020: Ajout 1: possibilite de choisir df correspondant au bassin versant + ajout 2: filtrer donnees selon type_exp_plav sans creer de nouvelles df

--- shiny_confinement.R
+++ shiny_confinement.R
@@ -1,9 +1,15 @@
 library(shiny)
+library(ggplot2)
 
 ui <- fluidPage(
   title = 'Algues Vertes',
   sidebarLayout(
     sidebarPanel(
+      
+      selectInput('bvav','Choix du BVAV',
+                  choices = c(names(df_bv),'Tous'),
+                  selected = 'Tous'),
+      
       selectInput('variable','Variable choisie',
                   choices = colnames(df_sphinx[2:101]),
                   selected =colnames(df_sphinx[2])),
@@ -23,24 +29,42 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-  table_reac <- reactive({
-    if ((input$variable == 'connaissance_service_plav')&(input$type_exp=='Toutes')){df_sphinx$connaissance_service_plav}
-    else if ((input$variable=='')
-             data <- data.frame(table(df_sphinx[,input$variable]))
-             colnames(data) <- c(input$variable,'Fréquence')
-             data
+  
+  ### Choix du data.frame selon le bassin versant
+  df_used <- reactive({
+    if (input$bvav %in% names(df_bv)){df_used <- df_bv[[input$bvav]]}
+    else if (input$bvav =='Tous'){df_used <- df_sphinx}
   })
-    plot <- reactive({
-      data <- data.frame(table(df_sphinx[,input$variable]))
-      colnames(data) <- c(input$variable,'Fréquence')
-      ggplot(data,aes(x=!!as.name(input$variable), y = `Fréquence`)) +
-        geom_col() +
-        theme_minimal()
-    })
-    output$table <- renderTable({table_reac()})
-    output$plot <- renderPlot({plot()})
+  
+  table_reac <- reactive({
+    
+    
+    ### Filtre sans créer les data frames en avance
+    ### selon la part de la surface agricole dans le bassin versant
+    if ((input$variable == 'connaissance_service_plav')&(input$type_exp=='Toutes')){data <- data.frame(table(df_used()[,input$variable]))}
+    else if ((input$variable=='connaissance_service_plav')&(input$type_exp %in% df_used()$type_exp_bv)){
+      data <- data.frame(table(filter(df_used(),type_exp_bv == input$type_exp)[,input$variable]))}
+    else {data <- data.frame(table(df_used()[,input$variable]))}
+    # data <- data.frame(table(df_used()[,input$variable]))
+    colnames(data) <- c(input$variable,'Fréquence')
+    data
+  })
+  plot <- reactive({
+    if ((input$variable == 'connaissance_service_plav')&(input$type_exp=='Toutes')){data <- data.frame(table(df_used()[,input$variable]))}
+    else if ((input$variable=='connaissance_service_plav')&(input$type_exp %in% df_used()$type_exp_bv)){
+      data <- data.frame(table(filter(df_used(),type_exp_bv == input$type_exp)[,input$variable]))}
+    else {data <- data.frame(table(df_used()[,input$variable]))}
+    # data <- data.frame(table(df_used()[,input$variable]))
+    colnames(data) <- c(input$variable,'Fréquence')
+    ggplot(data,aes(x=!!as.name(input$variable), y = `Fréquence`)) +
+      geom_col() +
+      theme_minimal()
+  })
+  output$table <- renderTable({table_reac()})
+  output$plot <- renderPlot({plot()})
 }
 
 
 shinyApp(ui, server)
+
 
